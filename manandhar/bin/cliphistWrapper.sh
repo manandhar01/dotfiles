6812265e48c025821164d841088bc427d91647f0
Bibek Manandhar <manandhar.bibek01@gmail.com>
2023-04-12 19:29:28 +0545

    timeout changed from 3000 to 5000 ms

--- manandhar/bin/cliphistWrapper.sh
+++ manandhar/bin/cliphistWrapper.sh
@@ -3,15 +3,15 @@
 if [ $1 = "select" ]; then
 	selected="$(cliphist list | wofi --dmenu | cliphist decode)"
 	wl-copy $selected
-	notify-send -t 3000 Selected "$selected"
+	notify-send -t 5000 Selected "$selected"
 fi
 
 if [ $1 = "delete" ]; then
 	selected="$(cliphist list | wofi --dmenu | tee >(cliphist decode) >(cliphist delete))"
-	notify-send -t 3000 Deleted "$selected"
+	notify-send -t 5000 Deleted "$selected"
 fi
 
 if [ $1 = "wipe" ]; then
 	cliphist wipe
-	notify-send -t 3000 "Clipboard Wiped"
+	notify-send -t 5000 "Clipboard Wiped"
 fi
