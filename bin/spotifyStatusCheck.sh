bc7b69284a67e9f6924ee715638ace1c26eed83a
Bibek Manandhar <manandhar.bibek01@gmail.com>
2024-08-04 17:49:59 +0545

    commented unused variable

--- bin/spotifyStatusCheck.sh
+++ bin/spotifyStatusCheck.sh
@@ -5,12 +5,12 @@
 # Function to get the currently playing track
 get_spotify_status() {
     # Check if Spotify is running
-    if pgrep -x "spotify" > /dev/null; then
+    if pgrep -x "spotify" >/dev/null; then
         # Fetch the status, title, artist, and mute status
         status=$(playerctl --player=spotify status 2>/dev/null)
         title=$(playerctl --player=spotify metadata title 2>/dev/null)
         artist=$(playerctl --player=spotify metadata artist 2>/dev/null)
-        muted=$(playerctl --player=spotify volume 2>/dev/null)
+        # muted=$(playerctl --player=spotify volume 2>/dev/null)
 
         # Determine class based on status
         if [ "$status" = "Playing" ]; then
@@ -33,4 +33,3 @@ get_spotify_status() {
 
 # Output the Spotify status in JSON format
 get_spotify_status
-
