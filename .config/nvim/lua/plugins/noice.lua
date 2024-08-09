7655d7eefcf7a027279053d347dc61fd46421401
Bibek Manandhar <manandhar.bibek01@gmail.com>
2024-08-09 21:31:15 +0545

    noice

---
+++ .config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,54 @@
+return {
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+		"rcarriga/nvim-notify",
+		"hrsh7th/nvim-cmp",
+		"nvim-treesitter/nvim-treesitter",
+	},
+	config = function()
+		require("noice").setup({
+			lsp = {
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+					["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+				},
+			},
+			-- you can enable a preset for easier configuration
+			presets = {
+				bottom_search = false, -- use a classic bottom cmdline for search
+				command_palette = true, -- position the cmdline and popupmenu together
+				long_message_to_split = true, -- long messages will be sent to a split
+				inc_rename = true, -- enables an input dialog for inc-rename.nvim
+				lsp_doc_border = true, -- add a border to hover docs and signature help
+			},
+
+			routes = {
+				{
+					filter = {
+						event = "msg_show",
+						kind = "",
+					},
+					opts = { skip = true },
+				},
+				{
+					filter = {
+						event = "msg_show",
+						kind = "search_count",
+					},
+					opts = { skip = true },
+				},
+			},
+		})
+
+		require("lualine").setup({
+			lualine_x = {
+				require("noice").api.statusline.mode.get,
+				cond = require("noice").api.statusline.mode.has,
+				color = { fg = "#ff9e64" },
+			},
+		})
+	end,
+}
