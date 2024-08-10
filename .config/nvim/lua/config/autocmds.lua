d7c675ea0e4752a304134e4e58195439f3c81488
Bibek Manandhar <manandhar.bibek01@gmail.com>
2024-08-10 14:56:45 +0545

    disabled comment continuation

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,1 +1,9 @@
 vim.api.nvim_set_hl(0, "Visual", { reverse = true })
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "*",
+	callback = function()
+		vim.opt.formatoptions:remove({ "c", "r", "o" })
+		vim.opt_local.formatoptions:remove({ "c", "r", "o" })
+	end,
+})
