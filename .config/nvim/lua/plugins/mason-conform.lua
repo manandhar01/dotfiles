18e7618bf605d991e992deaab5ef8a4742c8b9f5
Bibek Manandhar <manandhar.bibek01@gmail.com>
2024-09-27 20:34:48 +0545

    mason-conform

---
+++ .config/nvim/lua/plugins/mason-conform.lua
@@ -0,0 +1,9 @@
+return {
+	"zapling/mason-conform.nvim",
+	dependencies = { "williamboman/mason.nvim", "stevearc/conform.nvim" },
+	config = function()
+		require("mason-conform").setup({
+			-- ignore_install = {'some_formatter'}
+		})
+	end,
+}
