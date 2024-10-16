f0df9ed914b24e45425c8ae94f003d59c922cce1
Bibek Manandhar <manandhar.bibek01@gmail.com>
2024-10-16 11:36:07 +0545

    auto save config updated

--- .config/nvim/lua/plugins/auto-save.lua
+++ .config/nvim/lua/plugins/auto-save.lua
@@ -2,13 +2,10 @@ return {
 	"okuuva/auto-save.nvim",
 	config = function()
 		require("auto-save").setup({
-			execution_message = {
-				enabled = false,
-			},
 			trigger_events = {
 				immediate_save = { "BufLeave" },
 				defer_save = { "InsertLeave", "TextChanged", "FocusLost" },
-				cancel_defered_save = { "InsertEnter" },
+				cancel_deferred_save = { "InsertEnter" },
 			},
 			debounce_delay = 60000,
 		})
