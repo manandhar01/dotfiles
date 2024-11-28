4ba93460b7e5a2d08bbdf1e56d05ae0afd65477a
Bibek Manandhar <manandhar.bibek01@gmail.com>
2024-11-28 20:26:27 +0545

    color enhancement

---
+++ .config/nvim/lua/plugins/modes.lua
@@ -0,0 +1,18 @@
+return {
+	"mvllow/modes.nvim",
+	config = function()
+		require("modes").setup({
+			colors = {
+				copy = "#f5c359",
+				delete = "#c75c6a",
+				insert = "#78ccc5",
+				visual = "#fbf1c7",
+			},
+			line_opacity = 0.3,
+			set_cursor = true,
+			set_cursorline = true,
+			set_number = true,
+			ignore_filetypes = { "NvimTree", "TelescopePrompt" },
+		})
+	end,
+}
