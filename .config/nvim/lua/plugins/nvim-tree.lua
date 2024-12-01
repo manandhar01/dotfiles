22dd61ef1db09de89ae8a783c6e0383c8b8a5a1a
Bibek Manandhar <manandhar.bibek01@gmail.com>
2024-12-01 18:51:24 +0545

    keymaps updated

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -151,6 +151,6 @@ return {
 
         vim.api.nvim_set_keymap("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
         vim.api.nvim_set_keymap("n", "<leader>r", ":NvimTreeRefresh<CR>", { noremap = true, silent = true })
-        vim.api.nvim_set_keymap("n", "<leader>f", ":NvimTreeFindFile<CR>", { noremap = true, silent = true })
+        vim.api.nvim_set_keymap("n", "<leader><leader>f", ":NvimTreeFindFile<CR>", { noremap = true, silent = true })
     end,
 }
