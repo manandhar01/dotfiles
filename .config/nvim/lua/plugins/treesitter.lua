62630207a194074b6b036dcb14e9fc60a513a3c6
Bibek Manandhar <manandhar.bibek01@gmail.com>
2026-02-10 21:24:04 +0545

    fix: use autocmd to start treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    main = "nvim-treesitter.configs",
+    lazy = false,
     opts = {
         ensure_installed = {
             "bash",
